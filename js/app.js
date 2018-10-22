9d9fd7b78296030a5e10a0dbcb53d76acc86df82
Zach M <zachdtr@aol.com>
2018-10-21 18:25:02 -0600

    initial commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+const frames = document.querySelectorAll(".frame");
+const covers = document.querySelectorAll(".cover");
+const mainContent = document.querySelector(".content");
+const rhymeWords = document.querySelectorAll(".word_to_rhyme");
+const hiddenWords = document.querySelectorAll(".word_behind");
+
+frames.forEach((frame, index) => {
+	frame.addEventListener("click", (event) => {
+		covers[index].classList.toggle("clicked");
+		let rhymeIndex = Math.floor(index/4);
+		console.log(rhymeWords[rhymeIndex]);
+		console.log(hiddenWords[index]);
+			if(hiddenWords[index].textContent === rhymeWords[rhymeIndex].textContent) {
+				covers[index].parentElement.style.background = "#33FF33";
+			}else {covers[index].parentElement.style.background = "#FF3333";}
+	});
+});
+
+// rhymeWords.forEach((word) => {
+// 	console.log(word.textContent);
+// });
+
+// hiddenWords.forEach((word) => {
+// 	console.log(word.textContent);
+// })
+
+// frames.forEach((frame) => {
+// 	console.log(frame);
+// })
